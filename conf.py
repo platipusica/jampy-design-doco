fc1d25d84bbb87fc6def701fb541de66d4c6f6fc
platipusica <abd@physics.uwa.edu.au>
2022-08-01 14:31:10 +0800

    favi

--- conf.py
+++ conf.py
@@ -145,6 +145,10 @@ html_favicon = '_static/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'hacks.css',
+]
+
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
