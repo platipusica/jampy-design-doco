e59a3b164fe8324b81af803a81dc6f26b57bfd71
Platipus <babicdd@gmail.com>
2024-09-25 10:41:40 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,8 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_sitemap','sphinx_copybutton']
+#extensions = ['sphinx_sitemap','sphinx_copybutton']
+extensions = ['sphinx_copybutton']
 html_baseurl = 'https://jampyapplicationbuilder.com/tips/'
 
 # Add any paths that contain templates here, relative to this directory.
